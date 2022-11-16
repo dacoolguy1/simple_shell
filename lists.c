4c21fefdd12a0203b94b7ffca5a76ceee7e724b7
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2022-11-16 13:45:50 +0100

    Lists.c

--- lists.c
+++ lists.c
@@ -4,7 +4,7 @@
  * add_node - adds a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
- * @num: node index used by history
+ * @num: node index used by history.
  *
  * Return: size of list
  */
